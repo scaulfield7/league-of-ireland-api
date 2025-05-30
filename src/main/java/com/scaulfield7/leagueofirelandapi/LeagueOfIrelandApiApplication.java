3162ce9ae80a072ea9e541076349b00147044297
scaulfield7 <scaulfield@live.ie>
2025-05-30 02:56:33 +0200

    Initial project set-up

---
+++ src/main/java/com/scaulfield7/leagueofirelandapi/LeagueOfIrelandApiApplication.java
@@ -0,0 +1,13 @@
+package com.scaulfield7.leagueofirelandapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeagueOfIrelandApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeagueOfIrelandApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaulfield7/leagueofirelandapi/LeagueOfIrelandApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaulfield7.leagueofirelandapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeagueOfIrelandApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
