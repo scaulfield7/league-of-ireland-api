216f9893323d7d72395b11f7f13f8a5cbd6baf61
scaulfield7 <scaulfield@live.ie>
2025-06-09 17:04:30 +0100

    Add teamService

--- src/main/java/com/scaulfield7/leagueofirelandapi/api/controller/TeamController.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/api/controller/TeamController.java
@@ -1,8 +1,9 @@
 package com.scaulfield7.leagueofirelandapi.api.controller;
 
+import com.scaulfield7.leagueofirelandapi.service.TeamService;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TeamController {
-
+    private TeamService teamService;
 }
