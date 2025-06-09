ec320afddbc4e917cc80a829926c1f4aeebfa7b9
scaulfield7 <scaulfield@live.ie>
2025-06-09 16:56:20 +0100

    Add teams list

--- src/main/java/com/scaulfield7/leagueofirelandapi/service/TeamService.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/service/TeamService.java
@@ -1,8 +1,10 @@
 package com.scaulfield7.leagueofirelandapi.service;
 
+import com.scaulfield7.leagueofirelandapi.api.model.Team;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TeamService {
+    private List<Team> teams;
 
 }
