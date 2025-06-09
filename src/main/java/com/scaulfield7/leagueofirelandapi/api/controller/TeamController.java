2b79943bdc30221a43a3b7572b5ef60346081107
scaulfield7 <scaulfield@live.ie>
2025-06-09 17:06:21 +0100

    Add Autowired Spring annotation

--- src/main/java/com/scaulfield7/leagueofirelandapi/api/controller/TeamController.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/api/controller/TeamController.java
@@ -1,12 +1,14 @@
 package com.scaulfield7.leagueofirelandapi.api.controller;
 
 import com.scaulfield7.leagueofirelandapi.service.TeamService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TeamController {
     private TeamService teamService;
 
+    @Autowired
     public TeamController(TeamService teamService) {
         this.teamService = teamService;
     }
