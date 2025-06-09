8edd890ae999ec1d7746fcbe70cc5a77513f5d2f
scaulfield7 <scaulfield@live.ie>
2025-06-09 17:05:34 +0100

    Add TeamController constructor

--- src/main/java/com/scaulfield7/leagueofirelandapi/api/controller/TeamController.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/api/controller/TeamController.java
@@ -6,4 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TeamController {
     private TeamService teamService;
+
+    public TeamController(TeamService teamService) {
+        this.teamService = teamService;
+    }
 }
