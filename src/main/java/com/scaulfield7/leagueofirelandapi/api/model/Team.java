e8dca0c414848d754792fb9d292c36cbc092539b
scaulfield7 <scaulfield@live.ie>
2025-06-11 20:47:17 +0100

    Add Team variables

--- src/main/java/com/scaulfield7/leagueofirelandapi/api/model/Team.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/api/model/Team.java
@@ -1,5 +1,10 @@
 package com.scaulfield7.leagueofirelandapi.api.model;
 
 public class Team {
-
+    private int id;
+    private String name;
+    private int leagueRanking;
+    private String homePitch;
+    private String manager;
+    private String website;
 }
