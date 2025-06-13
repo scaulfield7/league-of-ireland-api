8049a9f2902cdb0aa9ab355bdf619330ed67e126
scaulfield7 <scaulfield@live.ie>
2025-06-13 17:51:56 +0100

    Add Bohemians

--- src/main/java/com/scaulfield7/leagueofirelandapi/service/TeamService.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/service/TeamService.java
@@ -14,5 +14,6 @@ public class TeamService {
         teams = new ArrayList<>();
 
         Team team1 = new Team(1, "Athlone Town AFC", 1, "Athlone Town Stadium", "Colin Fortune", "https://athlonetownafc.ie/");
+        Team team2 = new Team(2, "Bohemians FC", 8, "Dalymount Park", "Alban Hysa", "https://bohemians.ie");
     }
 }
