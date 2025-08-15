83c0f43fa29355ff8e5ac6e1c33ed94b4f8c5a0d
scaulfield7 <scaulfield@live.ie>
2025-08-15 19:08:18 +0100

    Add getTeamByName_validTeamName_returnsTeam test

--- src/test/java/com/scaulfield7/leagueofirelandapi/api/TeamControllerTest.java
+++ src/test/java/com/scaulfield7/leagueofirelandapi/api/TeamControllerTest.java
@@ -26,6 +26,24 @@ public class TeamControllerTest {
     }
 
     @Test
+    public void getTeamByName_validTeamName_returnsTeam() {
+        TeamService teamService = new TeamService();
+        String mockTeamName = "Athlone Town AFC";
+        Team expectedTeam = new Team(1, "Athlone Town AFC", 1, "Athlone Town Stadium", "Colin Fortune", "https://athlonetownafc.ie");
+        Team actualTeam = teamService.getTeamByName(mockTeamName).orElse(null);
+        if (actualTeam != null) {
+            assert actualTeam.getId() == expectedTeam.getId();
+            assert actualTeam.getName().equals(expectedTeam.getName());
+            assert actualTeam.getLeagueRanking() == expectedTeam.getLeagueRanking();
+            assert actualTeam.getHomePitch().equals(expectedTeam.getHomePitch());
+            assert actualTeam.getManager().equals(expectedTeam.getManager());
+            assert actualTeam.getWebsite().equals(expectedTeam.getWebsite());
+        } else {
+            throw new RuntimeException("Expected team not found");
+        }
+    }
+
+    @Test
     public void getAllTeams_validRequest_returnsCorrectNumberOfTeams() {
         TeamService teamService = new TeamService();
         int expectedNumberOfTeams = 12;
