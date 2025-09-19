1a6841f2f4dc4ad7dbe902a8661287487f2065d7
scaulfield7 <scaulfield@live.ie>
2025-09-19 18:13:25 +0100

    Add WebClientConfig.java class

---
+++ src/main/java/com/scaulfield7/leagueofirelandapi/config/WebClientConfig.java
@@ -0,0 +1,17 @@
+package com.scaulfield7.leagueofirelandapi.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    public WebClient webClient() {
+        WebClient webClient = new WebClient(BrowserVersion.CHROME);
+        webClient.getOptions().setThrowExceptionOnScriptError(false);
+        webClient.getOptions().setCssEnabled(false);
+        webClient.getOptions().setJavaScriptEnabled(false);
+        return webClient;
+    }
+}
