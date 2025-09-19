8dbe0b9850f357493721d92bc2c9b5743e2093b1
scaulfield7 <scaulfield@live.ie>
2025-09-19 18:15:42 +0100

    Add missing htmlunit imports

--- src/main/java/com/scaulfield7/leagueofirelandapi/config/WebClientConfig.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/config/WebClientConfig.java
@@ -1,5 +1,7 @@
 package com.scaulfield7.leagueofirelandapi.config;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.WebClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
