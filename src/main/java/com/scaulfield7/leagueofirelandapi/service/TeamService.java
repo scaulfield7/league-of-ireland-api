f9a37fdd246cfed67f2e34b62cc5e3fdcec496c3
scaulfield7 <scaulfield@live.ie>
2025-10-06 00:19:30 +0100

    Remove unused PropertySourcesPlaceholderConfigurer import

--- src/main/java/com/scaulfield7/leagueofirelandapi/service/TeamService.java
+++ src/main/java/com/scaulfield7/leagueofirelandapi/service/TeamService.java
@@ -3,7 +3,6 @@ package com.scaulfield7.leagueofirelandapi.service;
 import org.htmlunit.html.*;
 import com.scaulfield7.leagueofirelandapi.api.model.Team;
 import jakarta.annotation.PostConstruct;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.stereotype.Service;
 
 import java.util.*;
